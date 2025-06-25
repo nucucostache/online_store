e63353334cc70d170d33f79261d2ac803422a327
Marian Costache <nucu.costache@gmail.com>
2025-06-25 18:09:02 +0300

    Ziua 24.06
    Ce am facut in data de 24.06

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Account
+from django.http import HttpResponse
+from django.template import loader
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
 
-# Create your views here.
+
+
+def index(request):
+    template = loader.get_template("accounts/login.html")
+    return render(request, "accounts/login.html")
+
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')  # sau altă pagină după login
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form': form})
+
+def register(request):
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # Poți crea aici UserProfile automat dacă vrei
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
+def logout(request):
+    return HttpResponse('Va rog, sa iesiti de pe site!')
+
+
+
+
+#----------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
